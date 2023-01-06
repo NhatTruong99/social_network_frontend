a09f588c7634dadd569338c40b7d3fcb991dd784
truong <truong992001@gmail.com>
2023-01-06 16:46:41 +0700

    up code

--- src/App.js
+++ src/App.js
@@ -1,78 +1,192 @@
-import TodoList from "./components/TodoList";
-import Textfield from '@atlaskit/textfield';
-import Button from '@atlaskit/button';
-import {useState,useCallback,useEffect} from "react";
-import {v4} from 'uuid';
-const TODO_APP_STORAGE_KEY = 'TODO_APP';
+import React, { useState, useEffect ,useRef} from "react";
+import { Routes, Route, Link ,Outlet} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch } from "react-redux";
+import {io} from "socket.io-client";
+
+import AuthService from "./services/auth.service";
+import AuthVerify from "./common/auth-verify";
+import PrivateRoute from "./utils/PrivateRoute";
+import Event from "./utils/Event";
+
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import PostList from "./components/Post/PostList";
+import GroupList from "./components/Group/GroupList";
+import GroupCreate from "./components/Group/GroupCreate";
+import GroupPage from "./components/Group/GroupPage";
+import ListConversation from "./components/Conversation/ListConversation";
+import { setSocket } from "./redux/actions/SocketActions";
+
+import ProfileComponent from "./components/Profile/ProfileComponent";
+import Search from "./components/Search/Search";
+import GroupEdit from "./components/Group/GroupEdit";
+
+import Navbar from "./Navbar";
+import PostDetail from "./components/Post/PostDetail";
+import AddUserRole from './components/Admin/UserRole/AddUserRole';
+import EditUserRole from './components/Admin/UserRole/EditUserRole';
+import UserRoleDataTable from './components/Admin/UserRole/UserRoleDataTable';
+
+import AddGroup from './components/Admin/Group/AddGroup';
+import EditGroup from './components/Admin/Group/EditGroup';
+import GroupDataTable from './components/Admin/Group/GroupDataTable';
+import Chart from "./components/Admin/Statistics/Chart";
+import CountRow from './components/Admin/Statistics/CountRow';
+
+import NotificationList from "./components/Notification/NotificationList";
+import RequesterList from "./components/Friend/ListRequester";
+
+import AdminNavbar from "./adminNavbar";
+const socket = io.connect("ws://localhost:8900");
+
 function App() {
-  const [todoList,setTodoList] = useState([]); //array
-  const [textInput,setTextInput] = useState(""); //array
-
-  useEffect(()=>{
-    const storagedTodoList = window.localStorage.getItem(TODO_APP_STORAGE_KEY)
-    if(storagedTodoList!=null ){
-      setTodoList(JSON.parse(storagedTodoList))
-    }
-  },[]);
-
-  useEffect(() =>{
-    if (todoList.length > 0) window.localStorage.setItem(TODO_APP_STORAGE_KEY,JSON.stringify(todoList));
-  },[todoList]);
-
-
-  const onTextInputChange = useCallback((e) =>{
-    setTextInput(e.target.value);
-  },[]);
-
-  const onAddButtonClick = useCallback((e) =>{
-    //them textInput vao ds todoList
-   
-    setTodoList([{id:v4(),name:textInput,isdone:0}, ...todoList ]);
-    setTextInput("");
-  },[textInput,todoList]);
-
-
-  const onCheckBtnClick = useCallback((id) =>{
-// todo.id === id ? {...todo ,isdone:1}:todo 
-    setTodoList(prevState => prevState.map((todo) => {
-      if (todo.id === id){
-      return todo.isdone === 0 ? {...todo ,isdone:1}: {...todo ,isdone:0}  
-      }
-      return todo;
-     
-    }
-      ))
-      ;
-  },[]);
-
-  const onDeleteBtnClick = useCallback((id) =>{          
-        const temp = todoList.find(item => item.id === id);
-        const test = todoList.filter(function(item) {
-          return item !== temp
-        })
-
-        setTodoList(test)
-      
-        
-          
-      },[todoList]);
-      
-  return( 
-    <>   
-    <h3>Danh sách cần làm</h3>
-    <Textfield
-     name="add-todo" 
-     placeholder="Thêm việc cần làm..." 
-    elemAfterInput={
-      <Button isDisabled={!textInput} appearance='primary' onClick={onAddButtonClick} >Thêm</Button>
-    } 
-    value = {textInput}
-    onChange= {onTextInputChange}
-    ></Textfield>
-    <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} onDeleteBtnClick={onDeleteBtnClick}/>
-    </>
-  )
   
-}
+  const [currentUser, setCurrentUser] = useState(undefined);
 
+  const user = AuthService.getCurrentUser();
+
+  const dispatch = useDispatch();
+  dispatch(setSocket(socket));
+  
+  useEffect(() => {
+    
+    if (user) {
+      setCurrentUser(user);
+    }
+
+    Event.on("logout", () => {
+      logOut();
+    });
+
+    return () => {
+      Event.remove("logout");
+      socket.close();
+    };
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+    setCurrentUser(undefined);
+  };
+  
+  const AppLayout = () => (
+	<>
+	  <main className={"main--container"}>
+		<div className={"main--content"}>
+		<div className="theme-layout">
+		{ currentUser &&  <Navbar user={user} currentUser={currentUser} logOut={logOut}/>}
+
+		  <Outlet />
+		  </div>
+		</div>
+	  </main>
+	</>
+  );
+
+  const AdminLayout = () => (
+	<>
+	  <main className={"admin--container"}>
+		<div className={"admin--content"}>
+		<div className="wrapper">
+
+		<AdminNavbar />
+		  <Outlet />
+		</div>
+		</div>
+	  </main>
+	</>
+  );
+  return (
+	<>
+        <Routes>
+		<Route element={<AppLayout />} >
+				{
+				!currentUser
+				?<Route path="/" element={<Login/>} />
+				:<Route path="/" element={<PostList/>} />}
+				
+				<Route path="/register" element={<Register/>} />
+				<Route path="/posts" element={
+				<PrivateRoute>
+					<PostList />
+				</PrivateRoute>
+				} />
+
+				<Route path="/detail/post/:postID" element={
+					<PrivateRoute>
+					<PostDetail />
+					</PrivateRoute>
+				} />
+				<Route path="/groups" element={
+					<PrivateRoute>
+					<GroupList />
+					</PrivateRoute>
+				} />
+				<Route path="/group/create" element={
+					<PrivateRoute>
+					<GroupCreate />
+					</PrivateRoute>
+				} />
+				<Route path="/group/:id" element={
+					<PrivateRoute>
+					<GroupPage />
+					</PrivateRoute>
+				} />
+
+				<Route path="/group/:id/edit" element={
+					<PrivateRoute>
+						<GroupEdit />
+					</PrivateRoute>
+				} />
+
+				<Route path="/profile/:userID" element={
+					<PrivateRoute>
+					<ProfileComponent />
+					</PrivateRoute>
+				} />
+				<Route path="/conversation" element={
+					<PrivateRoute>
+					<ListConversation />
+					</PrivateRoute>
+				} />
+				<Route path="/search/:keyword" element={
+					<PrivateRoute>
+					<Search />
+					</PrivateRoute>
+				} />
+
+				<Route path="/list-requester/:userID" element={
+					<PrivateRoute>
+					<RequesterList />
+					</PrivateRoute>
+				}/>
+				
+		</Route>
+
+
+		<Route element={<AdminLayout />} >
+
+			<Route path="/admin/user-role/read" element={<UserRoleDataTable/>}/>
+			<Route path="/admin/user-role/create" element={<AddUserRole/>}/>W
+			<Route path="/admin/user-role/edit/:userID/:roleID" element={<EditUserRole/>}/>
+
+			<Route path="/admin/group/read" element={<GroupDataTable/>}/>
+			<Route path="/admin/group/create" element={<AddGroup/>}/>
+			<Route path="/admin/group/edit/:id" element={<EditGroup/>}/>
+
+		
+			<Route path="admin/chart" element={<Chart/>}/>
+			<Route path="admin/countRow" element={<CountRow/>}/>    
+		</Route>
+
+
+
+
+			
+        </Routes>
+        <AuthVerify logOut={logOut}/>
+     </>
+  );
+}
 export default App;
